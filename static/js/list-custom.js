dcdf76170bb22acf9903124e3d0932a9d78a27b5
Derek Severin <severinderek@gmail.com>
2025-05-26 14:28:50 +0700

    services link from other page fix

---
+++ static/js/list-custom.js
@@ -0,0 +1,18 @@
+
+function updateServivesLink() {
+    if (document.body.clientWidth < 768) {
+        nav_services.href = "#services";
+    } else {
+        nav_services.href = "#services_foreground";
+    }
+}
+
+window.addEventListener('load', (e) => {
+    updateServivesLink();
+});
+
+window.addEventListener('resize', (e) => {
+    updateServivesLink();
+});
+
+const nav_services = document.getElementById("nav_services_foreground");
